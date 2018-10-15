614b027eeeda1a4e5aa1f139e9fca6484a119466
Nurkaiyr <yedige21@gmail.com>
2018-10-15 14:27:45 +0600

    Initial commit

---
+++ app/src/main/java/com/example/a1/helloworld/ActivityTwo.java
@@ -0,0 +1,14 @@
+package com.example.a1.helloworld;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class ActivityTwo extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_two);
+    }
+
+}
